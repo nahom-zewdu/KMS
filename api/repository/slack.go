c5d8d516e8bd73a920651b354eb4bc72c849fd30
nahom-zewdu <nahiyo86@gmail.com>
2025-08-06 20:12:20 +0300

    feat: add a repository that creates a new slack repo and implememnts it's interface

---
+++ api/repository/slack.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/nahom-zewdu/kMS/api/domain"
+)
+
+type SlackRepo struct {
+	message domain.SlackMessage
+}
+
+func NewSlackRepo(message domain.SlackMessage) domain.SlackRepository {
+	return &SlackRepo{
+		message: message,
+	}
+}
+
+func (sr SlackRepo) GetMessage(ctx context.Context, id string) (*domain.SlackMessage, error) {
+
+	if sr.message.ID == id {
+		return &sr.message, nil
+	}
+
+	return nil, nil
+}
