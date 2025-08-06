8d52cc1b172ef1b1579d9d95b0ec6dc7817147fc
nahom-zewdu <nahiyo86@gmail.com>
2025-08-06 21:20:15 +0300

    fix: change the service argument type of SetupRoutes func from struct to interface defined in domain

--- api/handlers/route.go
+++ api/handlers/route.go
@@ -2,10 +2,10 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/nahom-zewdu/kMS/api/services"
+	"github.com/nahom-zewdu/kMS/api/domain"
 )
 
-func SetupRoutes(router *gin.Engine, service services.SlackService) {
+func SetupRoutes(router *gin.Engine, service domain.SlackService) {
 	slackHandler := NewSlackHandler(service)
 
 	// Define the route for getting a Slack message by ID
