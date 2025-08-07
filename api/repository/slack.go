3228523f3d4f58a5a7449e83cc1381c751e20962
nahom-zewdu <nahiyo86@gmail.com>
2025-08-07 18:42:30 +0300

    feat: add a repo func to handle initializing and  passing the payload data to be stored

--- api/repository/slack.go
+++ api/repository/slack.go
@@ -2,28 +2,31 @@ package repository
 
 import (
 	"context"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/nahom-zewdu/kMS/api/domain"
 )
 
 type SlackRepo struct {
-	message domain.SlackMessage
+	storage domain.StoragePort
 }
 
-func NewSlackRepo(message domain.SlackMessage) domain.SlackRepository {
-	return &SlackRepo{
-		message: message,
-	}
+func NewSlackRepo(storage domain.StoragePort) domain.SlackRepository {
+	return &SlackRepo{storage: storage}
 }
 
-func (sr *SlackRepo) GetMessage(ctx context.Context, id string) (*domain.SlackMessage, error) {
-	message := domain.SlackMessage{
-		ID:      id,
-		UserID:  "user123",
-		Text:    "This is a sample message",
-		Channel: "general",
+func (sr *SlackRepo) IngestRepo(ctx context.Context, data domain.IngestRequest) error {
+	id := uuid.New().String()
+	now := time.Now().UTC()
+
+	insertPayload := map[string]interface{}{
+		"id":         id,
+		"source":     data.Source,
+		"content":    data.Content,
+		"entity_id":  data.EntityID,
+		"created_at": now,
 	}
 
-	return &message, nil
-
+	return sr.storage.Insert(ctx, "raw_data", insertPayload)
 }
