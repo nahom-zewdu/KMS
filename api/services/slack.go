b65975a51a65ed5973136df12a74d91202efd6e4
nahom-zewdu <nahiyo86@gmail.com>
2025-08-08 06:00:04 +0300

    fix: set common fields of job payload on slack service layer before passing it for Ingestion and Publishing

--- api/services/slack.go
+++ api/services/slack.go
@@ -2,23 +2,43 @@ package services
 
 import (
 	"context"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/nahom-zewdu/kMS/api/domain"
 )
 
 type SlackService struct {
-	repo domain.SlackRepository
+	repo      domain.SlackRepository
+	publisher domain.Publisher
 }
 
-func NewSlackService(repo domain.SlackRepository) domain.SlackService {
+func NewSlackService(repo domain.SlackRepository, publisher domain.Publisher) domain.SlackService {
 	return &SlackService{
-		repo: repo,
+		repo:      repo,
+		publisher: publisher,
 	}
 }
 
 func (ss *SlackService) IngestService(ctx context.Context, data domain.IngestRequest) error {
+	var job domain.JobPayload
 
-	err := ss.repo.IngestRepo(ctx, data)
+	job.ID = "*"
+	job.RecordID = uuid.New().String()
+	job.Source = data.Source
+	job.Content = data.Content
+
+	if data.EntityID != "" {
+		job.EntityID = data.EntityID
+	}
+	job.CreatedAt = time.Now().UTC().Format(time.RFC3339)
+
+	err := ss.repo.IngestRepo(ctx, job)
+	if err != nil {
+		return err
+	}
+
+	err = ss.publisher.Publish(ctx, "slack_jobs", job)
 	if err != nil {
 		return err
 	}
