5d6ebd547e28c463b21f18a707e2d751ed1f446d
Nahom <nahiyo86@gmail.com>
2025-10-02 23:37:15 +0300

    fix(slackbot): remove bot mention and user ids from query string

--- api/services/slack_bot.go
+++ api/services/slack_bot.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"regexp"
 	"strings"
 
 	"github.com/nahom-zewdu/kMS/api/domain"
@@ -11,50 +12,55 @@ import (
 )
 
 type SlackBot struct {
-	client       *slack.Client
-	queryService domain.QueryService
-	signkey      string
+	client     *slack.Client
+	query      domain.QueryService
+	signingKey string
 }
 
-func NewSlackBot(token, signkey string, queryService domain.QueryService) domain.SlackBotService {
-	client := slack.New(token)
+func NewSlackBot(botToken, signingKey string, query domain.QueryService) domain.SlackBotService {
 	return &SlackBot{
-		client:       client,
-		signkey:      signkey,
-		queryService: queryService,
+		client:     slack.New(botToken),
+		query:      query,
+		signingKey: signingKey,
 	}
 }
 
-func (sb *SlackBot) HandleEvent(ctx context.Context, teamID, channelID, threadTs, query string) error {
+func (sb *SlackBot) HandleEvent(ctx context.Context, teamID, channel, threadTs, query string) error {
+	// Clean query (remove @KnowSphere and Slack user IDs)
 	cleanQuery := removeBotMention(query)
 	if cleanQuery == "" {
-		return nil // Ignore empty queries
-	}
-
-	// Process the query
-	resp, err := sb.queryService.HandleQuery(ctx, domain.QueryRequest{Query: cleanQuery})
-	if err != nil {
-		log.Printf("Query error for %s: %v", cleanQuery, err)
-		// Post Fallback message to Slack
-		_, _, err := sb.client.PostMessage(channelID,
-			slack.MsgOptionText("Sorry, I couldn't process your request at the moment.", false),
+		log.Printf("Empty query after cleaning for channel %s, thread %s", channel, threadTs)
+		_, _, err := sb.client.PostMessage(channel,
+			slack.MsgOptionText("Please provide a valid query (e.g., 'Who owns github?').", false),
 			slack.MsgOptionTS(threadTs))
-
 		return err
 	}
 
-	// Post response to Slack
-	_, _, err = sb.client.PostMessage(channelID,
+	// Call QueryService
+	resp, err := sb.query.HandleQuery(ctx, domain.QueryRequest{Query: cleanQuery})
+	if err != nil {
+		log.Printf("Query error for '%s': %v", cleanQuery, err)
+		_, _, err = sb.client.PostMessage(channel,
+			slack.MsgOptionText("Sorry, I couldn't process that query. Try again?", false),
+			slack.MsgOptionTS(threadTs))
+		return err
+	}
+
+	// Post response in thread
+	_, _, err = sb.client.PostMessage(channel,
 		slack.MsgOptionText(resp.Answer, false),
 		slack.MsgOptionTS(threadTs))
 	if err != nil {
-		log.Printf("Failed to post message to Slack: %v", err)
-		return fmt.Errorf("failed to post message to Slack: %w", err)
+		log.Printf("Failed to post Slack message: %v", err)
+		return fmt.Errorf("failed to post Slack message: %v", err)
 	}
+
 	return nil
 }
 
 func removeBotMention(query string) string {
-	// Simple cleanup: remove @KnowSphere or similar
-	return strings.TrimSpace(strings.Replace(query, "@kms", "", 1))
+	// Remove bot mention and Slack user IDs (e.g., <@U09HQ4SH58E>)
+	re := regexp.MustCompile(`<@U[0-9A-Z]+>|@knowsphere`)
+	cleaned := re.ReplaceAllString(query, "")
+	return strings.TrimSpace(cleaned)
 }
