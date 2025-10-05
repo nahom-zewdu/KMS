dff8eea019781b72c0fd2fe03c061315a82bc1ba
Nahom <nahiyo86@gmail.com>
2025-10-05 12:48:54 +0300

    chore: Add Redis Pub/Sub subscription for query answers (channel: query_results:query_id).

--- api/services/slack_bot.go
+++ api/services/slack_bot.go
@@ -8,26 +8,30 @@ import (
 	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/nahom-zewdu/kMS/api/domain"
+	"github.com/redis/go-redis/v9"
 	"github.com/slack-go/slack"
 )
 
 type SlackBot struct {
 	client       *slack.Client
 	slackService domain.SlackService
+	redis        *redis.Client
 	signingKey   string
 }
 
-func NewSlackBot(botToken, signingKey string, slackService domain.SlackService) domain.SlackBotService {
+func NewSlackBot(botToken, signingKey string, slackService domain.SlackService, redis *redis.Client) domain.SlackBotService {
 	return &SlackBot{
 		client:       slack.New(botToken),
 		slackService: slackService,
+		redis:        redis,
 		signingKey:   signingKey,
 	}
 }
 
 func (sb *SlackBot) HandleEvent(ctx context.Context, teamID, channel, threadTs, query string) error {
-	ctx, cancel := context.WithTimeout(ctx, 30*time.Second) // Extended timeout
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
 	cleanQuery := removeBotMention(query)
@@ -44,14 +48,36 @@ func (sb *SlackBot) HandleEvent(ctx context.Context, teamID, channel, threadTs, 
 		strings.Contains(cleanQuery, "?")
 
 	if isQuery {
-		answer, err := sb.queryNLP(ctx, cleanQuery)
+		queryID := uuid.New().String()
+		// Publish query to Redis Streams
+		job := domain.JobPayload{
+			ID:        "*",
+			RecordID:  queryID,
+			Source:    "slack",
+			Content:   cleanQuery,
+			CreatedAt: time.Now().UTC().Format(time.RFC3339),
+		}
+		err := sb.slackService.IngestService(ctx, domain.IngestRequest{
+			Source:  "slack",
+			Content: cleanQuery,
+		})
 		if err != nil {
-			log.Printf("NLP query error for '%s': %v", cleanQuery, err)
+			log.Printf("Ingest error for query '%s': %v", cleanQuery, err)
+		}
+
+		// Subscribe to Pub/Sub for answer
+		pubsub := sb.redis.Subscribe(ctx, "query_results:"+queryID)
+		defer pubsub.Close()
+		msg, err := pubsub.ReceiveMessage(ctx)
+		if err != nil {
+			log.Printf("Pub/Sub receive error for query '%s': %v", cleanQuery, err)
 			_, _, err = sb.client.PostMessage(channel,
 				slack.MsgOptionText("Sorry, I couldn't process that query. Try again?", false),
 				slack.MsgOptionTS(threadTs))
 			return err
 		}
+		answer := msg.Payload
+
 		_, _, err = sb.client.PostMessage(channel,
 			slack.MsgOptionText(answer, false),
 			slack.MsgOptionTS(threadTs))
@@ -67,11 +93,7 @@ func (sb *SlackBot) HandleEvent(ctx context.Context, teamID, channel, threadTs, 
 		msg := "Message recorded."
 		if err != nil {
 			log.Printf("Ingest error for '%s': %v", cleanQuery, err)
-			if strings.Contains(err.Error(), "failed to publish to Redis stream") {
-				msg = "Message recorded in database, but failed to queue for processing. Try again later."
-			} else {
-				msg = "Failed to record message. Try again?"
-			}
+			msg = "Failed to record message. Try again?"
 		}
 		_, _, err = sb.client.PostMessage(channel,
 			slack.MsgOptionText(msg, false),
@@ -86,16 +108,8 @@ func (sb *SlackBot) HandleEvent(ctx context.Context, teamID, channel, threadTs, 
 	return nil
 }
 
-func (sb *SlackBot) queryNLP(ctx context.Context, query string) (string, error) {
-	log.Printf("Simulating NLP query for: %s", query)
-	if strings.Contains(strings.ToLower(query), "who owns github") {
-		return "Nahom owns github, Jira #435", nil
-	}
-	return "No answer found.", nil
-}
-
 func removeBotMention(query string) string {
-	re := regexp.MustCompile(`(?i)<@U[0-9A-Z]+>|@kms`)
+	re := regexp.MustCompile(`(?i)<@U[0-9A-Z]+>|@KnowSphere`)
 	cleaned := re.ReplaceAllString(query, "")
 	reInvalid := regexp.MustCompile(`[^a-zA-Z0-9\s\?\.,#]`)
 	cleaned = reInvalid.ReplaceAllString(cleaned, "")
